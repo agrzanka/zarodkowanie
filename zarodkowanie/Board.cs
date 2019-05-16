be59784f80d13ed0621af7063919af5d07bf4045
Agata Grzanka <ag.grzanka@gmail.com>
2019-05-17 00:37:15 +0200

    basics of Cell and Board classes added

--- zarodkowanie/Board.cs
+++ zarodkowanie/Board.cs
@@ -4,9 +4,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace zarodkowanie
+namespace GameOfLife2D
 {
     class Board
     {
+        public int size;
+        public int boardH;
+        public Cell[,] cells;
+
+        public Board(int size, int boardH)
+        {
+            this.boardH = boardH;
+            this.size = size;
+
+
+            cells = new Cell[this.size, this.boardH];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int s = 0; s < boardH; s++)
+                {
+                    int[] idd = { i, s };
+                    this.cells[i, s] = new Cell(idd, false, size);
+                }
+            }
+
+            foreach (var c in cells)
+            {
+                c.setNeighbors(this.size, this.boardH);
+            }
+        }
+
+        
+
+        public void setup_const()
+        {
+            this.cells[1, 1].Life = true;
+            this.cells[2, 1].Life = true;
+            this.cells[1, 2].Life = true;
+            this.cells[2, 2].Life = true;
+        }
+
+
+        public void update()
+        {
+            List<int[]> nextRoundAlive = new List<int[]>();
+
+            foreach (var c in this.cells)
+            {
+                int alive = 0;
+                for (int n = 0; n < 8; n++)
+                {
+                    if (this.cells[c.neighbors[n, 0], c.neighbors[n, 1]].Life == true)
+                        alive++;
+                }
+                if (c.Life == true && (alive == 2 || alive == 3))
+                    nextRoundAlive.Add(c.id);
+                else if (c.Life == false && alive == 3)
+                    nextRoundAlive.Add(c.id);
+            }
+
+            foreach (var c in this.cells)
+            {
+                if (nextRoundAlive.Contains(c.id))
+                    c.Life = true;
+                else
+                    c.Life = false;
+            }
+        }
     }
 }
--- zarodkowanie/Cell.cs
+++ zarodkowanie/Cell.cs
@@ -4,9 +4,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace zarodkowanie
+namespace GameOfLife2D
 {
     class Cell
     {
+        public bool Life { get; set; }
+        public int[] id = new int[2];
+        public int[,] neighbors = new int[8, 2];
+
+
+        public Cell()
+        {
+            this.Life = false;
+            this.id[0] = 0;
+            this.id[1] = 0;
+        }
+
+        public Cell(int[] id, bool life, int boardSize)
+        {
+            this.id = id;
+            this.Life = life;
+
+        }
+        //periodical boundary conditions
+        public void setNeighbors(int boardSize, int boardH)
+        {
+            int x1, x2, y1, y2;
+
+            if (this.id[0] - 1 < 0)
+                x1 = boardSize - 1;
+            else x1 = this.id[0] - 1;
+
+            if (this.id[0] + 1 > boardSize - 1)
+                x2 = 0;
+            else x2 = this.id[0] + 1;
+
+            if (this.id[1] - 1 < 0)
+                y1 = boardH - 1;
+            else y1 = this.id[1] - 1;
+
+            if (this.id[1] + 1 > boardH - 1)
+                y2 = 0;
+            else y2 = this.id[1] + 1;
+
+
+            this.neighbors[0, 0] = x1;
+            this.neighbors[0, 1] = this.id[1];
+
+            this.neighbors[1, 0] = x1;
+            this.neighbors[1, 1] = y1;
+
+            this.neighbors[2, 0] = this.id[0];
+            this.neighbors[2, 1] = y1;
+
+            this.neighbors[3, 0] = x2;
+            this.neighbors[3, 1] = y1;
+
+            this.neighbors[4, 0] = x2;
+            this.neighbors[4, 1] = this.id[1];
+
+            this.neighbors[5, 0] = x2;
+            this.neighbors[5, 1] = y2;
+
+            this.neighbors[6, 0] = this.id[0];
+            this.neighbors[6, 1] = y2;
+
+            this.neighbors[7, 0] = x1;
+            this.neighbors[7, 1] = y2;
+        }
+
+        //non-periodical boundary condiditions:
+
+
     }
 }
